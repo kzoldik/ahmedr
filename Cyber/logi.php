5d8f32173a8cfe6f500a11e16794ee8fca5185ef
kzoldik <ahraddad58@gmail.com>
2025-05-09 10:33:12 +0300

    Update logi.php

--- Cyber/logi.php
+++ Cyber/logi.php
@@ -1,1 +1,30 @@
+<?php
+$correct_username = "ahmed";
+$correct_password = "ana";
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["username"]) && isset($_POST["password"])) {
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+        if ($username === $correct_username && $password === $correct_password) {
+            echo "Login successful! Welcome, " . htmlspecialchars($username);
+            // توجيه المستخدم إلى صفحة أخرى مثلاً:
+            // header("Location: welcome.php");
+        } else {
+            echo "Invalid username or password!";
+        }
+    } else {
+        echo "Please fill all fields!";
+    }
+}
+?>
+
+<center>
+        <h1>LOGIN PAGE</h1>
+        <form action="k.php" method="POST">
+            <input type="text" name="username" placeholder="Enter your name"> <br>
+            <input type="password" name="password" placeholder="Enter your password"> <br>
+            <button type="submit">LOGIN</button>
+        </form>
+</center>
